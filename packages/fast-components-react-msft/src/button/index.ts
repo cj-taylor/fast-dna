ce638f9dabb27f049d1ea4f711a8e557dc0cde0c
nicholasrice <nicholas.rice119@gmail.com>
2018-10-01 22:49:22 -0700

    correct exports of remaining components

--- packages/fast-components-react-msft/src/button/index.ts
+++ packages/fast-components-react-msft/src/button/index.ts
@@ -1,8 +1,5 @@
 import * as React from "react";
-import {
-    IButtonClassNameContract,
-} from "@microsoft/fast-components-react-base";
-import { IMSFTButtonClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
+import { IButtonClassNameContract } from "@microsoft/fast-components-class-name-contracts-msft";
 import manageJss, { IJSSManagerProps, JSSManagerProps } from "@microsoft/fast-jss-manager-react";
 import { ButtonStyles, IDesignSystem } from "@microsoft/fast-components-styles-msft";
 import MSFTButton, {
